88bbd4549d791f5f6ecca9b29f9d56cd0538d28c
Sigbjørn Skjæret <cisc@users.sf.net>
1999-12-09 00:44:34 +0000

    Removed write_ancillary_data() prototype. (No longer used)

--- l3bitstream-pvt.h
+++ l3bitstream-pvt.h
@@ -5,8 +5,12 @@
  * $Id$
  *
  * $Log$
- * Revision 1.1  1999/11/24 08:42:59  markt
- * Initial revision
+ * Revision 1.2  1999/12/09 00:44:34  cisc
+ * Removed write_ancillary_data() prototype. (No longer used)
+ *
+ * Revision 1.1.1.1  1999/11/24 08:42:59  markt
+ * initial checkin of LAME
+ * Starting with LAME 3.57beta with some modifications
  *
  * Revision 1.1  1996/02/14 04:04:23  rowlands
  * Initial revision
@@ -22,8 +26,6 @@ static int encodeSideInfo( III_side_info_t  *si );
 static void encodeMainData( int              l3_enc[2][2][576],
 			    III_side_info_t  *si,
 			    III_scalefac_t   *scalefac );
-
-static void write_ancillary_data( char *theData, int lengthInBits );
 
 static void drain_into_ancillary_data( int lengthInBits );
 
