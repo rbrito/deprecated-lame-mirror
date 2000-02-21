8bc6df0af0383d7932a022fc828d548c0f2fdc8f
Sigbjørn Skjæret <cisc@users.sf.net>
2000-02-21 23:58:07 +0000

    Make sure input-file is closed when CTRL-C is received.

--- amiga_mpega.c
+++ amiga_mpega.c
@@ -7,6 +7,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 1.3  2000/02/21 23:58:07  cisc
+ * Make sure input-file is closed when CTRL-C is received.
+ *
  * Revision 1.2  2000/01/11 20:23:41  cisc
  * Fixed timestatus bug (forgot nsamp variable).
  *
@@ -32,6 +35,8 @@ MPEGA_CTRL      mctrl;
 
 static int break_cleanup(void)
 {
+	CloseSndFile();
+
 	if(mstream) {
 		MPEGA_close(mstream);
 		mstream = NULL;
