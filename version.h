5883e740b312f1722424c5cf1ad4501bc2f9849f
Mark Taylor <markt@users.sf.net>
2000-05-06 22:05:12 +0000

    updated version for 3.80 beta release

--- version.h
+++ version.h
@@ -24,7 +24,7 @@
 
 #define LAME_MAJOR_VERSION	3	/* Major version number */
 #define LAME_MINOR_VERSION	80	/* Minor version number */
-#define LAME_ALPHAVERSION	1	/* Set number if this is an alpha version, otherwise zero */
+#define LAME_ALPHAVERSION	0	/* Set number if this is an alpha version, otherwise zero */
 #define LAME_BETAVERSION        0	/* Set number if this is a beta version, otherwise zero */
 
 #define PSY_MAJOR_VERSION	0	/* Major version number */
