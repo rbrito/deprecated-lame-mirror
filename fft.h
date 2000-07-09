e6b49bdb13633ee53171ebf93dbac41b8939aadd
Naoki Shibata <shibatch@users.sf.net>
2000-07-09 13:19:03 +0000

    I forgot to commit fft.h. (by Naoki Shibata)

--- fft.h
+++ fft.h
@@ -3,8 +3,8 @@
 
 #include "encoder.h"
 
-void fft_long(FLOAT x_real[BLKSIZE], int, short **);
-void fft_short(FLOAT x_real[3][BLKSIZE_s], int, short **);
-void init_fft(void);
+void fft_long(lame_global_flags *gfp, FLOAT x_real[BLKSIZE], int, short **);
+void fft_short(lame_global_flags *gfp, FLOAT x_real[3][BLKSIZE_s], int, short **);
+void init_fft(lame_global_flags *gfp);
 
 #endif
