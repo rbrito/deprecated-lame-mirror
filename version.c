c2aeb75c3341762f73fe1aa343c94736d06d30af
Sigbjørn Skjæret <cisc@users.sf.net>
2000-09-10 19:52:03 +0000

    correct includes for libsndfile prototypes

--- version.c
+++ version.c
@@ -22,7 +22,8 @@
 #include "version.h"    // macros of version numbers
 
 #ifdef LIBSNDFILE
-# include "lame.h"      // prototype of  sf_get_lib_version() ?
+# include "lame.h"
+# include "get_audio.h"      // includes sndfile.h for prototype of sf_get_lib_version()
 #endif
 
 void  lame_print_version ( FILE* fp )
