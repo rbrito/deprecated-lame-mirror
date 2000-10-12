76bd4169d3d7d7968a67e54a2b47ba4795b50fc9
Robert Hegemann <robert@users.sf.net>
2000-10-12 17:18:29 +0000

    CPU feature detection code now using NASM

--- libmp3lame/util.c
+++ libmp3lame/util.c
@@ -462,112 +462,31 @@ lame_errorf(const char * s, ...)
 
 int has_3DNow (void)
 {
-#if (defined(__GNUC__) && defined(__i386__)) 
-    __asm__ (
-        "pushal \n"
-        "pushfl \n"                             
-        "popl  %eax \n"
-        "movl  %eax,%ecx \n"
-        "xorl  $0x00200000,%eax \n"
-        "pushl %eax \n"
-        "popfl \n"
-        "pushfl \n"
-        "popl  %eax \n"
-        "cmpl  %ecx,%eax \n"
-        "jz    NO_CPUID1 \n"
-
-        "movl  $0x80000000,%eax \n"
-        "CPUID \n"
-        "cmpl  $0x80000000,%eax \n"
-        "jbe   NO_EXTENDED_MSR1 \n"
-
-        "movl  $0x80000001,%eax \n"
-        "CPUID \n"
-        "testl $0x80000000,%edx \n"
-        "jz    NO_3DNow1 \n"
-        "popal \n"
-        "movl  $1,%eax \n"
-        "jmp   return1 \n"
-    "NO_CPUID1: \n"
-    "NO_EXTENDED_MSR1: \n"
-    "NO_3DNow1: \n"
-        "popal \n"
-        "xorl  %eax,%eax \n"
-    "return1: \n"    
-    );
-    return;
+#ifdef HAVE_NASM 
+    extern int has_3DNow_nasm(void);
+    return has_3DNow_nasm();
 #else
-    return 0;
+    return 0;   /* don't know, assume not */
 #endif
 }    
 
 int has_MMX (void)
 {
-#if (defined(__GNUC__) && defined(__i386__)) 
-    __asm__ (
-        "pushal \n"
-        "pushfl \n"                             
-        "popl  %eax \n"
-        "movl  %eax,%ecx \n"
-        "xorl  $0x00200000,%eax \n"
-        "pushl %eax \n"
-        "popfl \n"
-        "pushfl \n"
-        "popl  %eax \n"
-        "cmpl  %ecx,%eax \n"
-        "jz    NO_CPUID2 \n"
-
-        "movl  $1,%eax \n"
-        "CPUID \n"
-        "testl $0x800000,%edx \n"
-        "jz    NO_MMX2 \n"
-        "popal \n"
-        "movl  $1,%eax \n"
-        "jmp   return2 \n"
-    "NO_CPUID2: \n"
-    "NO_MMX2: \n"
-        "popal \n"
-        "xorl  %eax,%eax \n"
-    "return2: \n"    
-    );
-    return;
+#ifdef HAVE_NASM 
+    extern int has_MMX_nasm(void);
+    return has_MMX_nasm();
 #else
-    return 0;
+    return 0;   /* don't know, assume not */
 #endif
 }    
 
 int has_SIMD (void)
 {
-#if (defined(__GNUC__) && defined(__i386__)) 
-    __asm__ (
-        "pushal \n"
-        "pushfl \n"                             
-        "popl  %eax \n"
-        "movl  %eax,%ecx \n"
-        "xorl  $0x00200000,%eax \n"
-        "pushl %eax \n"
-        "popfl \n"
-        "pushfl \n"
-        "popl  %eax \n"
-        "cmpl  %ecx,%eax \n"
-        "jz    NO_CPUID3 \n"
-
-        "movl  $1,%eax \n"
-        "CPUID \n"
-        "testl $0x2000000,%edx \n"
-        "jz    NO_SIMD3 \n"
-        "popal \n"
-        "movl  $1,%eax \n"
-        "jmp   return3 \n"
-    "NO_CPUID3: \n"
-    "NO_SIMD3: \n"
-        "popal \n"
-        "xorl  %eax,%eax \n"
-    "return3: \n"    
-    );
-    return;
+#ifdef HAVE_NASM 
+    extern int has_SIMD_nasm(void);
+    return has_SIMD_nasm();
 #else
-    return 0;
+    return 0;   /* don't know, assume not */
 #endif
 }    
 
