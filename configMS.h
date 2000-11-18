e7fa53d1f3f946165d2fb615c5aed07b5b8cd366
Alexander Leidinger <aleidinger@users.sf.net>
2000-11-18 10:48:20 +0000

    introduce configMS.h for Windows

---
+++ configMS.h
@@ -0,0 +1,18 @@
+/* The number of bytes in a unsigned long.  */
+#define SIZEOF_UNSIGNED_LONG 4
+
+/* Name of package */
+#define PACKAGE "lame"
+
+/* Version number of package */
+#define VERSION "3.88"
+
+/* Define if compiler has function prototypes */
+#define PROTOTYPES 1
+
+/* enable VBR bitrate histogram */
+#define BRHIST 1
+
+/* IEEE754 compatible machine */
+#define TAKEHIRO_IEEE754_HACK 1
+
