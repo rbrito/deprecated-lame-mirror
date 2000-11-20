5ae88f90eed2bcd93f31d8a883eec4f2f2fdbdc0
Robert Hegemann <robert@users.sf.net>
2000-11-20 20:45:33 +0000

    fix for mp3x.c: showed usage twice fix for parse.c: lame_version_print()   in some situations the text is longer than 80 characters   now the url will be wrapped into the next line, right aligned   if 80 character aren't enough

--- frontend/mp3x.c
+++ frontend/mp3x.c
@@ -43,11 +43,17 @@ int main(int argc, char **argv)
   lame_global_flags gf;  
   char outPath[MAX_NAME_SIZE];
   char inPath[MAX_NAME_SIZE];
+  int ret;
 
   lame_init_old(&gf);
-  if(argc==1)  usage(&gf, stderr, argv[0]);  /* no command-line args  */
-
-  parse_args(&gf,argc, argv, inPath, outPath); 
+  if(argc <=1 ) {
+    usage(&gf, stderr, argv[0]);  /* no command-line args  */
+    return -1;
+  }
+  ret = parse_args(&gf,argc, argv, inPath, outPath); 
+  if (ret < 0)
+    return ret == -2 ? 0 : 1;
+  
   gf.analysis=1;
 
   init_infile(&gf,inPath);
--- frontend/parse.c
+++ frontend/parse.c
@@ -49,8 +49,21 @@
 
 static int  lame_version_print ( FILE* const fp )
 {
-   fprintf ( fp, "LAME version %s (%s)\n\n", get_lame_version (), get_lame_url () );
-   return 0;
+    const char * v = get_lame_version ();
+    const char * u = get_lame_url ();
+    const int lenv = strlen (v);
+    const int lenu = strlen (u);
+    const int lw = 80; /* line width of terminal in characters */
+    const int sw = 16; /* static width of text */
+    
+    if (lw >= lenv+lenu+sw || lw < lenu+2)                
+        /* text fits in 80 chars per line, or line even too small for url */
+        fprintf ( fp, "LAME version %s (%s)\n\n", v, u );
+    else            
+        /* text too long, wrap url into next line, right aligned */
+        fprintf ( fp, "LAME version %s\n%*s(%s)\n\n", v, lw-2-lenu, "", u );
+    
+    return 0;
 }
 
 int  print_license ( const lame_global_flags* gfp, FILE* const fp, const char* ProgramName )  /* print version & license */
@@ -113,7 +126,7 @@ int  usage ( const lame_global_flags* gfp, FILE* const fp, const char* ProgramNa
               "\n"
               "Try  \"%s --help\"     for more information\n" 
               "  or \"%s --longhelp\"\n"
-              "  or \"%s --?\"        for a complete options list\n\n",
+              "  or \"%s -?\"         for a complete options list\n\n",
               ProgramName, ProgramName, ProgramName, ProgramName ); 
     return 0;
 }
