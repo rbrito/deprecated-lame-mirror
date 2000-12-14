3f4ecc738aef379e457afdd902f7d1dbdbf0f787
Alexander Leidinger <aleidinger@users.sf.net>
2000-12-14 10:04:34 +0000

    configure.in:  - determine the size of various types at configure time  - add --enable-debug=anoying
    configMS.h:
     - add SIZEOF_... defines, see configure.in
    
    Makefile.unix:
     - bugfix from Robert (shame on me)
    
    Makefile.DJGPP:
     - add cpu_feat.nas and scalar.nas to the build with NASM
    
    Submitted by Egor Duda <deo@logos-m.ru> (thank you, works great):
     * Makefile.am: add $(EXEEXT) to executables for cygwin build
     * Makefile.am: take source .wav file from srcdir
     * configure.in: require autoconf 2.13
     * configure.in: check for executable files extension
     * frontend/Makefile.am: use builddir to get autogenerated headers
       and libraries from, in case we're doing build in separate directory;
       take normal headers from srcdir instead of current.
     * libmp3lame/Makefile.am: ditto
     * libmp3lame/i386/Makefile.am: ditto
     * mpglib/Makefile.am: ditto

--- configMS.h
+++ configMS.h
@@ -1,8 +1,29 @@
+/* The number of bytes in a double.  */
+#define SIZEOF_DOUBLE 8
+
+/* The number of bytes in a float.  */
+#define SIZEOF_FLOAT 4
+
 /* The number of bytes in a int.  */
 #define SIZEOF_INT 4
 
+/* The number of bytes in a long.  */
+#define SIZEOF_LONG 4
+
+/* The number of bytes in a long double.  */
+#define SIZEOF_LONG_DOUBLE 12
+
+/* The number of bytes in a short.  */
+#define SIZEOF_SHORT 2
+
+/* The number of bytes in a unsigned int.  */
+#define SIZEOF_UNSIGNED_INT 4
+
 /* The number of bytes in a unsigned long.  */
 #define SIZEOF_UNSIGNED_LONG 4
+
+/* The number of bytes in a unsigned short.  */
+#define SIZEOF_UNSIGNED_SHORT 2
 
 /* Name of package */
 #define PACKAGE "lame"
