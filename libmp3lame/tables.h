03491808f14ce1bf0d883b266eb9651f0ec6b475
Alexander Leidinger <aleidinger@users.sf.net>
2000-12-20 15:14:12 +0000

    Patches for KLEMM_43 protected code

--- libmp3lame/tables.h
+++ libmp3lame/tables.h
@@ -23,7 +23,55 @@
 #define LAME_TABLES_H
 
 #include "machine.h"
+
+#ifdef KLEMM_43
+
+typedef struct {
+    unsigned char  no;
+    unsigned char  width;
+    unsigned char  minval_2;
+    float          quiet_thr;
+    float          norm;
+    float          bark;
+} type1_t;
+
+typedef struct {
+    unsigned char  no;
+    unsigned char  width;
+    float          quiet_thr;
+    float          norm;
+    float          SNR;
+    float          bark;
+} type2_t;
+
+typedef struct {
+    unsigned int  no     :  5;
+    unsigned int  cbw    :  3;
+    unsigned int  bu     :  6;
+    unsigned int  bo     :  6;
+    unsigned int  w1_576 : 10;
+    unsigned int  w2_576 : 10;
+} type34_t;
+
+typedef struct {
+    size_t                 len1;
+    const type1_t*  const  tab1;
+    size_t                 len2;
+    const type2_t*  const  tab2;
+    size_t                 len3;
+    const type34_t* const  tab3;
+    size_t                 len4;
+    const type34_t* const  tab4;
+} type5_t;
+
+extern const type5_t  table5 [6];
+
+#else /* KLEMM_43 */
+
 extern const FLOAT psy_data [];
+
+#endif /* KLEMM_43 */
+
 
 #define HTN	34
  
