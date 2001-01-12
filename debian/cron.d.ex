2e8b1ee9c411fa901b93fd3609c5b63676e5cde3
Mark Taylor <markt@users.sf.net>
2001-01-12 19:17:24 +0000

    New Debian package from Ingo Saitz

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lame package
-#
-0 4	* * *	root	lame_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lame):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="lame" command="/usr/bin/lame"
