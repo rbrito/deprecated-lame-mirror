26b4272c442efba2fd67887b0a86925d4bd3eabd
Alexander Leidinger <aleidinger@users.sf.net>
2001-02-20 18:17:59 +0000

    - doxygen parseable documentation   - better lclint flags   - enable get_lame_version_numerical()

--- libmp3lame/version.c
+++ libmp3lame/version.c
@@ -19,7 +19,20 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id$ */
+/*!
+  \file   version.c
+  \brief  Version numbering for LAME.
+
+  Contains functions which describe the version of LAME.
+
+  \author A.L. Faber
+  \version \$Id$
+  \ingroup libmp3lame
+
+  \todo The mp3x version should be located in the mp3x source,
+        not in the libmp3lame source.
+*/
+
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -27,13 +40,16 @@
 
 
 #include <stdio.h>
+#include <lame.h>
 #include "version.h"    /* macros of version numbers */
 
 #ifdef WITH_DMALLOC
 #include <dmalloc.h>
 #endif
 
+//! Stringify \a x.
 #define STR(x)   #x
+//! Stringify \a x, perform macro expansion.
 #define XSTR(x)  STR(x)
 
 #if defined(MMX_choose_table)
@@ -54,114 +70,133 @@
 # define V3  ""
 #endif
 
+//! Compile time features.
 #define V   V1 V2 V3
 
-
+//! Get the LAME version string.
+/*!
+  \param void
+  \return a pointer to a string which describes the version of LAME.
+*/
 const char*  get_lame_version ( void )		/* primary to write screen reports */
 {
     /* Here we can also add informations about compile time configurations */
 
-    /*@-observertrans@*/
 #if   LAME_ALPHA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(LAME_MAJOR_VERSION) "." XSTR(LAME_MINOR_VERSION) " " V
         "(alpha " XSTR(LAME_ALPHA_VERSION) ", " __DATE__ " " __TIME__ ")";
 #elif LAME_BETA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(LAME_MAJOR_VERSION) "." XSTR(LAME_MINOR_VERSION) " " V
         "(beta " XSTR(LAME_BETA_VERSION) ", " __DATE__ ")";
 #else
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(LAME_MAJOR_VERSION) "." XSTR(LAME_MINOR_VERSION) " " V;
 #endif
 
-    /*@-statictrans@*/
     return str;
-    /*@=statictrans =observertrans@*/
 }
 
 
-const char*  get_lame_short_version ( void )	/* primary to write into MP3 files */
-{
-    /* adding date and time to version string makes it harder for output validation */
+//! Get the short LAME version string.
+/*!
+  It's mainly for inclusion into the MP3 stream.
 
-    /*@-observertrans@*/
+  \param void   
+  \return a pointer to the short version of the LAME version string.
+*/
+const char*  get_lame_short_version ( void )
+{
+    /* adding date and time to version string makes it harder for output
+       validation */
+
 #if   LAME_ALPHA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(LAME_MAJOR_VERSION) "." XSTR(LAME_MINOR_VERSION) " (alpha)";
 #elif LAME_BETA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(LAME_MAJOR_VERSION) "." XSTR(LAME_MINOR_VERSION) " (beta)";
 #else
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(LAME_MAJOR_VERSION) "." XSTR(LAME_MINOR_VERSION)
 #endif
 
-    /*@-statictrans@*/
     return str;
-    /*@=statictrans =observertrans@*/
 }
 
 
+//! Get the version string for GPSYCHO.
+/*!
+  \param void
+  \return a pointer to a string which describes the version of GPSYCHO.
+*/
 const char*  get_psy_version ( void )
 {
-    /*@-observertrans@*/
 #if   PSY_ALPHA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(PSY_MAJOR_VERSION) "." XSTR(PSY_MINOR_VERSION)
         " (alpha " XSTR(PSY_ALPHA_VERSION) ", " __DATE__ " " __TIME__ ")";
 #elif PSY_BETA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(PSY_MAJOR_VERSION) "." XSTR(PSY_MINOR_VERSION)
         " (beta " XSTR(PSY_BETA_VERSION) ", " __DATE__ ")";
 #else
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(PSY_MAJOR_VERSION) "." XSTR(PSY_MINOR_VERSION);
 #endif
 
-    /*@-statictrans@*/
     return str;
-    /*@=statictrans =observertrans@*/
 }
 
 
+//! Get the mp3x version string.
+/*!
+  \param void
+  \return a pointer to a string which describes the version of mp3x.
+*/
 const char*  get_mp3x_version ( void )
 {
-    /*@-observertrans@*/
 #if   MP3X_ALPHA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(MP3X_MAJOR_VERSION) "." XSTR(MP3X_MINOR_VERSION)
         " (alpha " XSTR(MP3X_ALPHA_VERSION) ", " __DATE__ " " __TIME__ ")";
 #elif MP3X_BETA_VERSION > 0
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(MP3X_MAJOR_VERSION) "." XSTR(MP3X_MINOR_VERSION)
         " (beta " XSTR(MP3X_BETA_VERSION) ", " __DATE__ ")";
 #else
-    static const char *const str =
+    static /*@observer@*/ const char *const str =
         XSTR(MP3X_MAJOR_VERSION) "." XSTR(MP3X_MINOR_VERSION);
 #endif
 
-    /*@-statictrans@*/
     return str;
-    /*@=statictrans =observertrans@*/
 }
 
 
+//! Get the URL for the LAME website.
+/*!
+  \param void
+  \return a pointer to a string which is a URL for the LAME website.
+*/
 const char*  get_lame_url ( void )
 {
-    /*@-observertrans@*/
-    static const char *const str = LAME_URL;
-    /*@-statictrans@*/
+    static /*@observer@*/ const char *const str = LAME_URL;
+
     return str;
-    /*@=statictrans =observertrans@*/
 }    
 
-#if 0
+
+//! Get the numerical representation of the version.
+/*!
+  Writes the numerical representation of the version of LAME and
+  GPSYCHO into lvp.
+
+  \param lvp    
+*/
 void get_lame_version_numerical ( lame_version_t *const lvp )
 {
-    /*@-observertrans@*/
-    static const char *const features = V;
-    /*@=observertrans@*/
+    static /*@observer@*/ const char *const features = V;
 
     /* generic version */
     lvp->major = LAME_MAJOR_VERSION;
@@ -176,10 +211,9 @@ void get_lame_version_numerical ( lame_version_t *const lvp )
     lvp->psy_beta  = PSY_BETA_VERSION;
 
     /* compile time features */
-    /*@-observertrans -mustfree -statictrans@*/
+    /*@-mustfree@*/
     lvp->features = features;
-    /*@=statictrans =mustfree =observertrans@*/
+    /*@=mustfree@*/
 }
-#endif
 
 /* end of version.c */
