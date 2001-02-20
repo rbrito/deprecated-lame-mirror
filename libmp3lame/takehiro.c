d8e2ec79b86104d81bb2f63211dec65a02452762
Alexander Leidinger <aleidinger@users.sf.net>
2001-02-20 18:19:37 +0000

    - CPU_features is now a struct

--- libmp3lame/takehiro.c
+++ libmp3lame/takehiro.c
@@ -977,7 +977,7 @@ void huffman_init(lame_internal_flags * const gfc)
     gfc->choose_table = choose_table_nonMMX;
     
 #ifdef MMX_choose_table
-    if (gfc->CPU_features_MMX) {
+    if (gfc->CPU_features.MMX) {
         extern int choose_table_MMX(const int *ix, const int *end, int *s);
         gfc->choose_table = choose_table_MMX;
     }
