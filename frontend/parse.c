05599788d982f53da869a584bd13ce8f52ee83fd
Mark Taylor <markt@users.sf.net>
2001-02-25 21:28:42 +0000

    removed VBR warning

--- frontend/parse.c
+++ frontend/parse.c
@@ -1234,11 +1234,6 @@ int  parse_args ( lame_global_flags* gfp, int argc, char** argv, char* const inP
 	    return -1;
 	}
     }
-    /* please check with Mark Taylor before removing this comment: */
-    if (gfp->VBR !=0 && gfp->VBR !=vbr_abr) {
-	fprintf(stderr,"\n**** Warning ****  VBR modes are under heavy development!\n");
-	fprintf(stderr,"In many cases, CBR and ABR will produce better results.\n\n");
-    }
     return 0;
 }
 
