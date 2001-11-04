6b6a38918eecfe3e99983de228556c9f130d3021
Robert Hegemann <robert@users.sf.net>
2001-11-04 18:50:29 +0000

    removed MAXQUANTERROR precompiler define

--- libmp3lame/quantize.h
+++ libmp3lame/quantize.h
@@ -23,8 +23,7 @@
 #define LAME_QUANTIZE_H
 
 #include "util.h"
-/*#include "l3side.h"
-*/
+
 void iteration_loop( lame_global_flags *gfp,
                      FLOAT8 pe[2][2], FLOAT8 ms_ratio[2], 
 		     FLOAT8 xr_org[2][2][576], III_psy_ratio ratio[2][2],
@@ -42,33 +41,5 @@ void ABR_iteration_loop( lame_global_flags *gfp,
 		     FLOAT8 xr_org[2][2][576], III_psy_ratio ratio[2][2],
 		     int l3_enc[2][2][576], 
 		     III_scalefac_t scalefac[2][2]);
-
-/*
- *  vbrquantize.c
- */
-
-void VBR_quantize( 
-                    lame_global_flags      *gfp,
-		    FLOAT8         pe[2][2], 
-                    FLOAT8         ms_ener_ratio[2],
-		    FLOAT8         xr[2][2][576], 
-                    III_psy_ratio  ratio[2][2],
-		    int            l3_enc[2][2][576],
-		    III_scalefac_t scalefac[2][2]);
-
-
-    /*  used by VBR_iteration_loop() */ 
-
-int VBR_noise_shaping2(
-                    lame_global_flags        *gfp,
-                    FLOAT8           xr       [576], 
-                    FLOAT8           xr34orig [576], 
-                    int              l3_enc   [576], 
-                    int              minbits, 
-                    int              maxbits,
-                    III_scalefac_t * const scalefac, 
-                    III_psy_xmin   * const l3_xmin,
-                    int              gr,
-                    int              ch );
 
 #endif /* LAME_QUANTIZE_H */
