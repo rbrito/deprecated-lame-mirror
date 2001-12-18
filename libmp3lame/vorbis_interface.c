80e17fbfb0a9f72e958d17d42b8ad368242f47b6
Alexander Leidinger <aleidinger@users.sf.net>
2001-12-18 11:13:01 +0000

    Try to unbreak vorbis support (it can't get worser). You may need the vorbis source and add -I/path/to/vorbis_src/includes/ to CFLAGS prior to running configure
    Tested by: Jonathan Ryshpan <jonrysh@pacbell.net> on RedHat 7.2
    Submitted by: Cyrille Lefevre <clefevre@citeweb.net> via FreeBSD problem report

--- libmp3lame/vorbis_interface.c
+++ libmp3lame/vorbis_interface.c
@@ -1,22 +1,5 @@
 /* $Id$ */
 
-
-/* Compile lame with
-
-#! /bin/bash
-
-OGGVORBIS_ROOT=/home/cvs/vorbis
-
-export CPPFLAGS="-I${OGGVORBIS_ROOT}/ogg/include -I${OGGVORBIS_ROOT}/vorbis/lib"
-
-make clean
-../configure
-make
-
-You can also do this with the "--with-vorbis" options in configure.
-
- */
-
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
@@ -27,8 +10,11 @@ You can also do this with the "--with-vorbis" options in configure.
 #include <stdlib.h>
 #include <limits.h>
 #include <time.h>
-#include "vorbis/codec.h"
-//#include "modes/modes.h"
+#include <vorbis/codec.h>
+#include <vorbis/vorbisenc.h>
+#include "codec_internal.h"
+#include "registry.h"
+#include "modes/modes.h"
 #include "lame.h"
 #include "util.h"
 
@@ -209,7 +195,7 @@ int lame_decode_ogg_fromfile( lame_global_flags*  gfp,
 {
   lame_internal_flags *gfc = gfp->internal_flags;
   int samples,result,i,j,eof=0,eos=0,bout=0;
-  double **pcm;
+  sample_t **pcm;
 
   while(1){
 
@@ -349,31 +335,67 @@ vorbis_block      vb2;  // local working space for packet->PCM decode
 int lame_encode_ogg_init(lame_global_flags *gfp)
 {
   lame_internal_flags *gfc=gfp->internal_flags;
+#ifdef THIS_CODE_IS_NOT_BROKEN_ANYMORE     
   char comment[MAX_COMMENT_LENGTH+1];
+#endif
 
   
   /********** Encode setup ************/
   
   /* choose an encoding mode */
   /* (mode 0: 44kHz stereo uncoupled, roughly 128kbps VBR) */
-  if (gfp->compression_ratio < 5.01) {
-    memcpy(&vi2,&info_E,sizeof(vi2));
-    MSGF( gfc, "Encoding with Vorbis mode info_E \n" );
-  } else if (gfp->compression_ratio < 6) {
-    memcpy(&vi2,&info_D,sizeof(vi2));
-    MSGF( gfc, "Encoding with Vorbis mode info_D \n" );
-  } else if (gfp->compression_ratio < 8) {
-    memcpy(&vi2,&info_C,sizeof(vi2));
-    MSGF( gfc, "Encoding with Vorbis mode info_C \n" );
-  } else if (gfp->compression_ratio < 10) {
-    memcpy(&vi2,&info_B,sizeof(vi2));
-    MSGF( gfc, "Encoding with Vorbis mode info_B \n" );
-  } else if (gfp->compression_ratio < 12) {
-    memcpy(&vi2,&info_A,sizeof(vi2));
-    MSGF( gfc, "Encoding with Vorbis mode info_A \n" );
-  } else {
-    memcpy(&vi2,&info_A,sizeof(vi2));
-    MSGF( gfc, "Encoding with Vorbis mode info_A \n" );
+  switch (gfc->channels_out) {
+  case 2:
+    if (gfp->compression_ratio < 5.01) {
+      memcpy(&vi2,&info_44c_E,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_E \n" );
+    } else if (gfp->compression_ratio < 6) {
+      memcpy(&vi2,&info_44c_D,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_D \n" );
+    } else if (gfp->compression_ratio < 8) {
+      memcpy(&vi2,&info_44c_C,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_C \n" );
+    } else if (gfp->compression_ratio < 10) {
+      memcpy(&vi2,&info_44c_B,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_B \n" );
+    } else if (gfp->compression_ratio < 12) {
+      memcpy(&vi2,&info_44c_A,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_A \n" );
+    } else if (gfp->compression_ratio < 14) {
+      memcpy(&vi2,&info_44c_X,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_X \n" );
+    } else if (gfp->compression_ratio < 16) {
+      memcpy(&vi2,&info_44c_Y,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_Y \n" );
+    } else if (gfp->compression_ratio < 18) {
+      memcpy(&vi2,&info_44c_Z,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_Z \n" );
+    } else {
+      memcpy(&vi2,&info_44c_A,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44c_A \n" );
+    }
+    break;
+
+  default:
+    if (gfp->compression_ratio < 10) {
+      memcpy(&vi2,&info_44_B,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44_B \n" );
+    } else if (gfp->compression_ratio < 12) {
+      memcpy(&vi2,&info_44_A,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44_A \n" );
+    } else if (gfp->compression_ratio < 14) {
+      memcpy(&vi2,&info_44_X,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44_X \n" );
+    } else if (gfp->compression_ratio < 16) {
+      memcpy(&vi2,&info_44_Y,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44_Y \n" );
+    } else if (gfp->compression_ratio < 18) {
+      memcpy(&vi2,&info_44_Z,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44_Z \n" );
+    } else {
+      memcpy(&vi2,&info_44_A,sizeof(vi2));
+      MSGF( gfc, "Encoding with Vorbis mode info_44_A \n" );
+    }
   }
 
   vi2.channels = gfc->channels_out;
@@ -517,7 +539,7 @@ int  lame_encode_ogg_frame (
     int  bytes = 0;
   
     /* expose the buffer to submit data */
-    double **buffer = vorbis_analysis_buffer(&vd2,gfp->framesize);
+    float **buffer = vorbis_analysis_buffer(&vd2,gfp->framesize);
   
     /* change level of input by -90 dB (no de-interleaving!) */
     for ( i = 0; i < gfp->framesize; i++ )
