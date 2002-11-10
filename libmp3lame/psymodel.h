3e0f42a9a9b2ba649b2faea291ec059a62a29248
Takehiro TOMINAGA <takehiro@users.sf.net>
2002-11-10 08:23:15 +0000

    fix warning

--- libmp3lame/psymodel.h
+++ libmp3lame/psymodel.h
@@ -48,12 +48,12 @@
 #define NS_PREECHO_ATT1 0.6
 #define NS_PREECHO_ATT2 0.3
 
-void psycho_analysis( lame_global_flags *gfc,
+void psycho_analysis( lame_global_flags *gfp,
 		      const sample_t *buffer[2],
 		      FLOAT ms_ener_ratio_d[2],
 		      III_psy_ratio masking[2][2] );
 
 int psymodel_init(lame_global_flags *gfp);
-
+void init_mask_add_max_values(lame_internal_flags *gfc);
 
 #endif /* LAME_PSYMODEL_H */
