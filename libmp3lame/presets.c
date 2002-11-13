4082af8bf12e413d5536b9619f6bc6539a4ff991
Gabriel Bouvigne <bouvigne@users.sf.net>
2002-11-13 20:24:04 +0000

    rollback of lowpass change before release

--- libmp3lame/presets.c
+++ libmp3lame/presets.c
@@ -20,10 +20,9 @@
 
 #include "lame.h"
 
-#ifdef HAVE_CONFIG_H
-# include <config.h>
-#endif
-#include "util.h"
+
+#define         Min(A, B)       ((A) < (B) ? (A) : (B))
+#define         Max(A, B)       ((A) > (B) ? (A) : (B))
 
 
 
@@ -37,6 +36,7 @@ int apply_abr_preset(lame_global_flags*  gfp, int preset)
         int    abr_kbps;
         int    expZ;
         int    expX;
+        int    lowpass;
         int    safejoint;
         double nsmsfix;
         double nsbass;
@@ -46,33 +46,62 @@ int apply_abr_preset(lame_global_flags*  gfp, int preset)
 
     // Switch mappings for ABR mode
     const dm_abr_presets_t abr_switch_map [] = {
-        // kbps Z  X safejoint nsmsfix ns-bass scale
-        {   8,  1, 6,  0,        0   ,   -3,      0.85 }, //   8 //impossible to use in stereo
-        {  16,  1, 6,  0,        0   ,   -3,      0.85 }, //  16
-        {  24,  1, 1,  0,        0   ,   -3,      0.85 }, //  24
-        {  32,  1, 1,  0,        0   ,   -3,      0.85 }, //  32
-        {  40,  1, 1,  0,        0   ,   -3,      0.85 }, //  40
-        {  48,  1, 1,  0,        0   ,   -3,      0.85 }, //  48
-        {  56,  1, 1,  0,        0   ,   -3,      0.85 }, //  56
-        {  64,  1, 1,  0,        0   ,   -3,      0.85 }, //  64
-        {  80,  1, 1,  0,        0   ,   -3,      0.85 }, //  80
-        {  96,  1, 1,  0,        0   ,   -4,      0.85 }, //  96
-        { 112,  1, 1,  0,        0   ,   -5,      0.87 }, // 112
-        { 128,  1, 1,  0,        0   ,   -6,      0.93 }, // 128
-        { 160,  1, 1,  0,        0   ,   -4,      0.95 }, // 160
-        { 192,  1, 1,  1,        1.7 ,   -2,      0.97 }, // 192
-        { 224,  1, 1,  1,        1.25,    0,      0.98 }, // 224
-        { 256,  0, 3,  1,        0   ,    0,      1.00 }, // 256
-        { 320,  0, 3,  1,        0   ,    0,      1.00 }  // 320
+        // kbps Z  X  lowpass safejoint nsmsfix ns-bass scale
+        {   8,  1, 6,  2000,  0,        0   ,   -3,      0.85 }, //   8 //impossible to use in stereo
+        {  16,  1, 6,  3700,  0,        0   ,   -3,      0.85 }, //  16
+        {  24,  1, 1,  3900,  0,        0   ,   -3,      0.85 }, //  24
+        {  32,  1, 1,  5500,  0,        0   ,   -3,      0.85 }, //  32
+        {  40,  1, 1,  7000,  0,        0   ,   -3,      0.85 }, //  40
+        {  48,  1, 1,  7500,  0,        0   ,   -3,      0.85 }, //  48
+        {  56,  1, 1, 10000,  0,        0   ,   -3,      0.85 }, //  56
+        {  64,  1, 1, 11000,  0,        0   ,   -3,      0.85 }, //  64
+        {  80,  1, 1, 13500,  0,        0   ,   -3,      0.85 }, //  80
+        {  96,  1, 1, 15300,  0,        0   ,   -4,      0.85 }, //  96
+        { 112,  1, 1, 16000,  0,        0   ,   -5,      0.87 }, // 112
+        { 128,  1, 1, 17500,  0,        0   ,   -6,      0.93 }, // 128
+        { 160,  1, 1, 18000,  0,        0   ,   -4,      0.95 }, // 160
+        { 192,  1, 1, 19500,  1,        1.7 ,   -2,      0.97 }, // 192
+        { 224,  1, 1, 20000,  1,        1.25,    0,      0.98 }, // 224
+        { 256,  0, 3, 20500,  1,        0   ,    0,      1.00 }, // 256
+        { 320,  0, 3, 21000,  1,        0   ,    0,      1.00 }  // 320
                                        };
 
-    
     // Variables for the ABR stuff
-    int r;
     int actual_bitrate = preset;
 
-    r= nearestBitrateFullIndex(preset);
+    int lower_range = 0, lower_range_kbps = 0,
+        upper_range = 0, upper_range_kbps = 0;
+    
+    int r; // r = resolved range
 
+    int b;
+
+
+
+
+    // We assume specified bitrate will be 320kbps
+    upper_range_kbps = abr_switch_map[16].abr_kbps;
+    upper_range = 16;
+    lower_range_kbps = abr_switch_map[16].abr_kbps;
+    lower_range = 16;
+ 
+    // Determine which significant bitrates the value specified falls between,
+    // if loop ends without breaking then we were correct above that the value was 320
+    for (b = 1; b < 17; b++) {
+        if ((Max(actual_bitrate, abr_switch_map[b].abr_kbps)) != actual_bitrate) {
+              upper_range_kbps = abr_switch_map[b].abr_kbps;
+              upper_range = b;
+              lower_range_kbps = abr_switch_map[b-1].abr_kbps;
+              lower_range = (b-1);
+              break; // We found upper range 
+        }
+    }
+
+    // Determine which range the value specified is closer to
+    if ((upper_range_kbps - actual_bitrate) > (actual_bitrate - lower_range_kbps))
+        r = lower_range;
+    else
+        r = upper_range;
 
 
     lame_set_VBR(gfp, vbr_abr); 
@@ -86,6 +115,7 @@ int apply_abr_preset(lame_global_flags*  gfp, int preset)
     lame_set_experimentalZ(gfp, abr_switch_map[r].expZ);
     lame_set_experimentalX(gfp, abr_switch_map[r].expX);
     lame_set_quality(gfp, 2);
+    lame_set_lowpassfreq(gfp, abr_switch_map[r].lowpass);
     lame_set_mode(gfp, JOINT_STEREO);
 
     if (abr_switch_map[r].safejoint > 0)
