d8f0570298b5ed295cff05ae351916e54bd82213
Takehiro TOMINAGA <takehiro@users.sf.net>
2002-12-15 09:11:33 +0000

    fix -q0 convined with --substep x option.

--- libmp3lame/lame.c
+++ libmp3lame/lame.c
@@ -368,7 +368,8 @@ lame_init_qval(lame_global_flags * gfp)
         gfc->psymodel = 1;
         gfc->quantization = 1;
         gfc->noise_shaping = 1; /* 2=usually lowers quality */
-        gfc->substep_shaping = 2;
+        if (gfc->substep_shaping != 0)
+	    gfc->substep_shaping = 2;
         gfc->noise_shaping_amp = 2;
         gfc->noise_shaping_stop = 1;
         gfc->use_best_huffman = 2;
