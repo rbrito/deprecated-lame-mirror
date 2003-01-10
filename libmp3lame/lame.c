c8be41ebf86a77c45f84b11293b16de4fb88542a
Takehiro TOMINAGA <takehiro@users.sf.net>
2003-01-10 18:57:28 +0000

    fix some initilization when ABR. (It seems ATH-adjust type 3 does not works fine with ABR mode ...)

--- libmp3lame/lame.c
+++ libmp3lame/lame.c
@@ -719,7 +719,7 @@ lame_init_params(lame_global_flags * const gfp)
 	gfp->quality = LAME_DEFAULT_QUALITY;
 
     gfc->sfb21_extra = 0;
-    if (gfp->VBR) {
+    if (gfp->VBR == vbr) {
         if (gfp->quality > 7) {
             gfp->quality = 7;     // needs psymodel
             ERRORF(gfc, "VBR needs a psymodel, switching to quality level 7\n");
