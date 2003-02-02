ed9868a873aed55c6b4b52d2fa04dcc63c423f62
Takehiro TOMINAGA <takehiro@users.sf.net>
2003-02-02 17:30:54 +0000

    keep id3tag when reencoding mp3(experimental). 1. it sometimes mis-recognize the tag. (but this is the same behavior of madplay) 2. we should add some option to enable/disable this function.
    after 2 is solved, I will backport it to the head branch.

--- frontend/main.c
+++ frontend/main.c
@@ -457,6 +457,17 @@ lame_encoder(lame_global_flags * gf, FILE * outf, int nogap, char *inPath,
 
     fwrite(mp3buffer, 1, imp3, outf);
 
+    /* id3 tag */
+    if (input_format == sf_mp3) {
+#define ID3TAGSIZE 128
+	extern FILE * musicin;
+	char id3tag[ID3TAGSIZE];
+	fseek(musicin, -ID3TAGSIZE, SEEK_CUR);
+	fread(id3tag, 1, ID3TAGSIZE, musicin);
+	if (memcmp(id3tag, "TAG", 3) == 0) {
+	    fwrite(id3tag, 1, ID3TAGSIZE, outf);
+	}
+    }
     return 0;
 }
 
