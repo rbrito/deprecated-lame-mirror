4c02237576a67eb35c83881102d4e944c9b7537a
Alexander Leidinger <aleidinger@users.sf.net>
2003-08-16 10:26:01 +0000

    Work around a bug which shows up if the input is a FIFO.
    Tested by:	Ville Herva <vherva@niksula.hut.fi>

--- frontend/get_audio.c
+++ frontend/get_audio.c
@@ -317,7 +317,11 @@ get_audio_common( lame_global_flags * const gfp,
      */
     if (count_samples_carefully) {
         remaining = tmp_num_samples - Min(tmp_num_samples, num_samples_read);
-        if (remaining < framesize)
+        if (remaining < framesize && 0 != remaining)
+            /* in case the input is a FIFO (at least it's reproducible with
+               a FIFO) tmp_num_samples may be 0 and therefore remaining
+               would be 0, but we need to read some samples, so don't
+               change samples_to_read to the wrong value in this case */
             samples_to_read = remaining;
     }
 
