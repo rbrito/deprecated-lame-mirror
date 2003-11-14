5f9aa78b50f6f844f5e67edb6fae45054030f375
Gabriel Bouvigne <bouvigne@users.sf.net>
2003-11-14 16:13:52 +0000

    use presets by default when using vbr-mtrh

--- libmp3lame/lame.c
+++ libmp3lame/lame.c
@@ -930,6 +930,8 @@ lame_init_params(lame_global_flags * const gfp)
         
     case vbr_mtrh: {
 
+        apply_preset(gfp, 500 - (gfp->VBR_q*10), 0);
+        
         if (gfp->quality < 0) gfp->quality = LAME_DEFAULT_QUALITY;
         if (gfp->quality > 7) {
             gfp->quality = 7;     /* needs psymodel */
@@ -941,80 +943,20 @@ lame_init_params(lame_global_flags * const gfp)
         if (gfp->cwlimit <= 0) gfp->cwlimit = 0.42 * gfp->out_samplerate;
         gfc->PSY->tonalityPatch = 1;
 
-        switch ( gfp->quant_comp ) {
-        default:
-        case 0: {
-                static const float dbQ[10]={-2.,-1.0,-.66,-.33,0.,0.33,.66,1.0,1.33,1.66};
-                gfc->VBR->quality = 0;
-                gfc->PSY->mask_adjust = dbQ[gfp->VBR_q];
-                gfc->PSY->mask_adjust_short = dbQ[gfp->VBR_q];
-                gfc->VBR->smooth = 1;
-            } 
-            break;        
-        case 1: {
-                static float const dbQ[10] = { -2., -1.4, -.7, 0, .7, 1.5, 2.3, 3.1, 4., 5 };
-                gfc->VBR->quality = 1;
-                gfc->PSY->mask_adjust = dbQ[gfp->VBR_q];
-                gfc->PSY->mask_adjust_short = dbQ[gfp->VBR_q];
-                gfc->VBR->smooth = 0;    
-            } 
-            break;        
-        case 2: {
-                static float const dbQ[10] = { -1., -.6, -.3, 0, 1, 2, 3, 4, 5, 6};
-                gfc->VBR->quality = 2;
-                gfc->PSY->mask_adjust = dbQ[gfp->VBR_q];
-                gfc->PSY->mask_adjust_short = dbQ[gfp->VBR_q];
-                gfc->VBR->smooth = 0;    
-                gfc->PSY->tonalityPatch = 0;
-            } 
-            break;        
-        case 3: {
-                static const float dbQ[10]={-2.,-1.0,-.66,-.33,0.,0.33,.66,1.0,1.33,1.66};
-                gfc->VBR->quality = 3;
-                gfc->PSY->mask_adjust = dbQ[gfp->VBR_q];
-                gfc->PSY->mask_adjust_short = dbQ[gfp->VBR_q];
-                gfc->VBR->smooth = 1;
-            } 
-            break;        
-        case 4: {
-                static float const dbQ[10] = { -6,-4.75,-3.5,-2.25,-1,.25,1.5,2.75,4,5.25 };
-                gfc->VBR->quality = 4;
-                gfc->PSY->mask_adjust = dbQ[gfp->VBR_q];
-                gfc->PSY->mask_adjust_short = dbQ[gfp->VBR_q];
-                gfc->VBR->smooth = 1;   /* not finally */
-            }
-            break;        
-        case 5: {
-                static const float dbQ[10]={-2.,-1.0,-.66,-.33,0.,0.33,.66,1.0,1.33,1.66};
-                gfc->VBR->quality = 0;
-                gfc->PSY->mask_adjust = dbQ[gfp->VBR_q];
-                gfc->PSY->mask_adjust_short = dbQ[gfp->VBR_q];
-                gfc->VBR->smooth = 2;
-            } 
-            break;        
-        case 9: {
-                static float const dbQ[10] = { -6,-4.75,-3.5,-2.25,-1,.25,1.5,2.75,4,5.25 };
-                gfc->VBR->quality = 4;
-                gfc->PSY->mask_adjust = dbQ[gfp->VBR_q];
-                gfc->PSY->mask_adjust_short = dbQ[gfp->VBR_q];
-                gfc->VBR->smooth = 0;   /* not finally */
-            }
-            break;        
-        }
-
-        if ( gfp->psymodel == PSY_NSPSYTUNE ) {
-            gfc->PSY->mask_adjust = gfp->maskingadjust;
-            gfc->PSY->mask_adjust_short = gfp->maskingadjust_short;
-            if (gfp->VBR_smooth < 0)
-                gfp->VBR_smooth = 0;
-            gfc->VBR->smooth = gfp->VBR_smooth;
-        }
+        gfc->PSY->mask_adjust = gfp->maskingadjust;
+        gfc->PSY->mask_adjust_short = gfp->maskingadjust_short;
+        if (gfp->VBR_smooth < 0)
+            gfp->VBR_smooth = 0;
+        gfc->VBR->smooth = gfp->VBR_smooth;
 
         
-        if (gfp->experimentalY)
+        /*  sfb21 extra only with MPEG-1 at higher sampling rates
+         */
+        if ( gfp->experimentalY )
             gfc->sfb21_extra = 0;
-        else
-            gfc->sfb21_extra = (gfp->out_samplerate > 36000);
+        else 
+            gfc->sfb21_extra = (gfp->out_samplerate > 44000);
+
         
         if ( gfp->athaa_type < 0 )
             gfc->ATH->use_adjust = 3;
