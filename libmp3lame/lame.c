5e8a7e2c82f76f6a161a6200ba3d382f5afbd1c7
Takehiro TOMINAGA <takehiro@users.sf.net>
2004-01-10 16:14:38 +0000

    fix the display of lowpass fileter config.

--- libmp3lame/lame.c
+++ libmp3lame/lame.c
@@ -1202,7 +1202,7 @@ lame_print_config(const lame_global_flags * gfp)
                  "Using polyphase highpass filter, transition band: %5.0f Hz - %5.0f Hz\n",
                  0.5 * gfc->highpass1 * out_samplerate,
                  0.5 * gfc->highpass2 * out_samplerate);
-        if (gfc->lowpass1 > 0.) {
+        if (0. < gfc->lowpass1 && gfc->lowpass1 < 1.) {
             MSGF
                 (gfc,
                  "Using polyphase lowpass  filter, transition band: %5.0f Hz - %5.0f Hz\n",
