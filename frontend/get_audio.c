9174a5b98653c5a0247051ff0c2753a29dea6c25
Takehiro TOMINAGA <takehiro@users.sf.net>
2004-01-24 17:28:47 +0000

    sync with the main branch. - remove message output during reading AIFF file.

--- frontend/get_audio.c
+++ frontend/get_audio.c
@@ -1138,8 +1138,6 @@ parse_aiff_header(lame_t  gfc, FILE * sf)
 
                 if (aiff_info.sampleSize == 16)
                   pcmswapbytes = (!swapbytes == (dataType == IFF_ID_2CLE));
-
-                fprintf(stderr, "swapbytes: %d\n", pcmswapbytes);
             }
             
             if (fskip(sf, (long) subSize, SEEK_CUR) != 0)
