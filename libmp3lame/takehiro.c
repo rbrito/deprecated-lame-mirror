816fcde34070187f4f9c5cf4959df5569adf4b27
Gabriel Bouvigne <bouvigne@users.sf.net>
2004-02-04 20:16:53 +0000

    fixed a crash in quantize_xrpow occuring when using low bitrates (32kbps)

--- libmp3lame/takehiro.c
+++ libmp3lame/takehiro.c
@@ -147,8 +147,11 @@ static void quantize_xrpow(const FLOAT8 *xp, int *pi, FLOAT8 istep, gr_info * co
                 int usefullsize;
                 usefullsize = cod_info->max_nonzero_coeff - j +1;
                 memset(&pi[cod_info->max_nonzero_coeff],0,
-                    sizeof(int)*(575-cod_info->max_nonzero_coeff));
+                    sizeof(int)*(576-cod_info->max_nonzero_coeff));
                 l = usefullsize >> 1;
+
+                /* no need to compute higher sfb values */
+                sfb = sfbmax + 1;
             }
 
             remaining = l%2;
@@ -192,7 +195,8 @@ static void quantize_xrpow(const FLOAT8 *xp, int *pi, FLOAT8 istep, gr_info * co
 	            xp += 2;
             }
         }
-        j += cod_info->width[sfb];
+        if (sfb <= sfbmax)
+            j += cod_info->width[sfb];
     }
 }
 
