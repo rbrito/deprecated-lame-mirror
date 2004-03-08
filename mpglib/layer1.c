64bf5ca1f72b0db6007ddb49b30f949383542ae9
Takehiro TOMINAGA <takehiro@users.sf.net>
2004-03-08 04:13:43 +0000

    fix layer1 compilation problem.

--- mpglib/layer1.c
+++ mpglib/layer1.c
@@ -24,7 +24,8 @@
 #include <dmalloc.h>
 #endif
 
-void I_step_one(unsigned int balloc[], unsigned int scale_index[2][SBLIMIT],struct frame *fr)
+static void
+I_step_one(PMPSTR mp, unsigned int balloc[], unsigned int scale_index[2][SBLIMIT],struct frame *fr)
 {
   unsigned int *ba=balloc;
   unsigned int *sca = (unsigned int *) scale_index;
@@ -34,38 +35,39 @@ void I_step_one(unsigned int balloc[], unsigned int scale_index[2][SBLIMIT],stru
     int i;
     int jsbound = fr->jsbound;
     for (i=0;i<jsbound;i++) { 
-      *ba++ = getbits(4);
-      *ba++ = getbits(4);
+      *ba++ = getbits(mp,4);
+      *ba++ = getbits(mp,4);
     }
     for (i=jsbound;i<SBLIMIT;i++)
-      *ba++ = getbits(4);
+      *ba++ = getbits(mp,4);
 
     ba = balloc;
 
     for (i=0;i<jsbound;i++) {
       if ((*ba++))
-        *sca++ = getbits(6);
+        *sca++ = getbits(mp,6);
       if ((*ba++))
-        *sca++ = getbits(6);
+        *sca++ = getbits(mp,6);
     }
     for (i=jsbound;i<SBLIMIT;i++)
       if ((*ba++)) {
-        *sca++ =  getbits(6);
-        *sca++ =  getbits(6);
+        *sca++ =  getbits(mp,6);
+        *sca++ =  getbits(mp,6);
       }
   }
   else {
     int i;
     for (i=0;i<SBLIMIT;i++)
-      *ba++ = getbits(4);
+      *ba++ = getbits(mp,4);
     ba = balloc;
     for (i=0;i<SBLIMIT;i++)
       if ((*ba++))
-        *sca++ = getbits(6);
+        *sca++ = getbits(mp,6);
   }
 }
 
-void I_step_two(real fraction[2][SBLIMIT],unsigned int balloc[2*SBLIMIT],
+static void
+I_step_two(PMPSTR mp, real fraction[2][SBLIMIT],unsigned int balloc[2*SBLIMIT],
 	unsigned int scale_index[2][SBLIMIT],struct frame *fr)
 {
   int i,n;
@@ -82,13 +84,13 @@ void I_step_two(real fraction[2][SBLIMIT],unsigned int balloc[2*SBLIMIT],
     ba = balloc;
     for (sample=smpb,i=0;i<jsbound;i++)  {
       if ((n = *ba++))
-        *sample++ = getbits(n+1);
+        *sample++ = getbits(mp,n+1);
       if ((n = *ba++))
-        *sample++ = getbits(n+1);
+        *sample++ = getbits(mp,n+1);
     }
     for (i=jsbound;i<SBLIMIT;i++) 
       if ((n = *ba++))
-        *sample++ = getbits(n+1);
+        *sample++ = getbits(mp,n+1);
 
     ba = balloc;
     for (sample=smpb,i=0;i<jsbound;i++) {
@@ -118,7 +120,7 @@ void I_step_two(real fraction[2][SBLIMIT],unsigned int balloc[2*SBLIMIT],
     ba = balloc;
     for (sample=smpb,i=0;i<SBLIMIT;i++)
       if ((n = *ba++))
-        *sample++ = getbits(n+1);
+        *sample++ = getbits(mp,n+1);
     ba = balloc;
     for (sample=smpb,i=0;i<SBLIMIT;i++) {
       if((n=*ba++))
@@ -142,16 +144,16 @@ int do_layer1(PMPSTR mp, unsigned char *pcm_sample,int *pcm_point)
   int i,channels = fr->channels;
   int single = fr->single;
 
-  fr->jsbound = (fr->mode == MPG_MD_JOINT_CHANNELS) ? (fr->mode_ext<<2)+4 : 32;
+  fr->jsbound = (fr->mode == MPG_MD_JOINT_STEREO) ? (fr->mode_ext<<2)+4 : 32;
 
   if(channels == 1 || single == 3)
     single = 0;
 
-  I_step_one(balloc,scale_index,fr);
+  I_step_one(mp,balloc,scale_index,fr);
 
   for (i=0;i<SCALE_BLOCK;i++)
   {
-    I_step_two(fraction,balloc,scale_index,fr);
+    I_step_two(mp,fraction,balloc,scale_index,fr);
 
     if(single >= 0)
     {
