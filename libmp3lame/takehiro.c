a88e2174a53ac3f92dce4e75e0c59f6d0d9a3de8
Gabriel Bouvigne <bouvigne@users.sf.net>
2004-05-26 21:04:35 +0000

    fixed a bug in quantize_xrpow, but it is still crashing Oddly, cbr/abr are working fine, and low vbr settings are also working fine. -V2 is not working

--- libmp3lame/takehiro.c
+++ libmp3lame/takehiro.c
@@ -346,7 +346,7 @@ static void quantize_xrpow(const FLOAT8 *xp, int *pi, FLOAT8 istep, gr_info * co
         assert( cod_info->width[sfb] >= 0 );
         if (prev_data_use && (prev_noise->step[sfb] == step)){
             /* do not recompute this part*/
-            iData += cod_info->width[sfb];
+            xp += cod_info->width[sfb];
             iData += cod_info->width[sfb];
         } else {
             int l;
