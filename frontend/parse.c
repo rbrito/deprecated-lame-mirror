483ef61c871b8a0b51554a8033ef9417485ea91e
Takehiro TOMINAGA <takehiro@users.sf.net>
2004-11-29 04:51:36 +0000

    remove unused definitions

--- frontend/parse.c
+++ frontend/parse.c
@@ -33,13 +33,7 @@
 # include <string.h>
 #else
 # ifndef HAVE_STRCHR
-#  define strchr index
 #  define strrchr rindex
-# endif
-char *strchr (), *strrchr ();
-# ifndef HAVE_MEMCPY
-#  define memcpy(d, s, n) bcopy ((s), (d), (n))
-#  define memmove(d, s, n) bcopy ((s), (d), (n))
 # endif
 #endif
 
