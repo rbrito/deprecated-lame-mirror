a8759f7b9d5c4f51e8d2eaa9a1b6cdd44c1ca745
Takehiro TOMINAGA <takehiro@users.sf.net>
2004-12-12 18:08:26 +0000

    use MDCT filter in -q5 and the better.

--- libmp3lame/lame.c
+++ libmp3lame/lame.c
@@ -86,42 +86,42 @@ init_qval(lame_t gfc)
 
     case 5: /* same as the default setting(-q 2) before LAME 3.83 */
 	/* LAME4's new default */
-	gfc->filter_type = 0;
+	gfc->filter_type = 1;
 	gfc->psymodel = 2;
 	gfc->noise_shaping_amp = 1;
 	gfc->use_best_huffman = 1;
 	break;
 
     case 4:
-	gfc->filter_type = 0;
+	gfc->filter_type = 1;
 	gfc->psymodel = 2;
 	gfc->noise_shaping_amp = 2;
 	gfc->use_best_huffman = 1;
 	break;
 
     case 3:
-	gfc->filter_type = 0;
+	gfc->filter_type = 1;
 	gfc->psymodel = 2;
 	gfc->noise_shaping_amp = 3;
 	gfc->use_best_huffman = 1;
 	break;
 
     case 2:	/* aliased -h */
-	gfc->filter_type = 0;
+	gfc->filter_type = 1;
 	gfc->psymodel = 2;
 	gfc->noise_shaping_amp = 4;
 	gfc->use_best_huffman = 1;
 	break;
 
     case 1:	/* LAME 3.94's -h + alpha */
-        gfc->filter_type = 0;
+        gfc->filter_type = 1;
         gfc->psymodel = 2;
         gfc->noise_shaping_amp = 4;
         gfc->use_best_huffman = 2; /* inner loop, PAINFULLY SLOW */
         break;
 
     case 0:	/* same with -q 1 */
-        gfc->filter_type = 0; /* 1 not yet coded */
+        gfc->filter_type = 1; /* 2 not yet coded */
         gfc->psymodel = 2;
         gfc->noise_shaping_amp = 4; /* 5 not yet coded */
         gfc->use_best_huffman = 2;
