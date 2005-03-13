d2f712c1ef1e253d3db775860b2e7fe53f54b4fb
Robert Hegemann <robert@users.sf.net>
2005-03-13 14:43:21 +0000

    version

--- ACM/ACM.cpp
+++ ACM/ACM.cpp
@@ -327,9 +327,9 @@ ACM::ACM( HMODULE hModule )
 	}
 
 #if   LAME_ALPHA_VERSION > 0
-	wsprintf(VersionString,"%s - %d.%d (alpha %d)", ACM_VERSION, LAME_MAJOR_VERSION, LAME_MINOR_VERSION,LAME_ALPHA_VERSION);
+	wsprintf(VersionString,"%s - %d.%d (alpha %d)", ACM_VERSION, LAME_MAJOR_VERSION, LAME_MINOR_VERSION,LAME_PATCH_VERSION);
 #elif LAME_BETA_VERSION > 0
-	wsprintf(VersionString,"%s - %d.%d (beta %d)", ACM_VERSION, LAME_MAJOR_VERSION, LAME_MINOR_VERSION, LAME_BETA_VERSION);
+	wsprintf(VersionString,"%s - %d.%d (beta %d)", ACM_VERSION, LAME_MAJOR_VERSION, LAME_MINOR_VERSION, LAME_PATCH_VERSION);
 #else
 	wsprintf(VersionString,"%s - %d.%d (stable)", ACM_VERSION, LAME_MAJOR_VERSION, LAME_MINOR_VERSION);
 #endif
