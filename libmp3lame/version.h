97437ff06ceff86c9aa1c48473dc83b7d84a174b
Takehiro TOMINAGA <takehiro@users.sf.net>
2005-04-02 07:28:49 +0000

    Now alpha 14. - Dramatical VBR speed improvement.   10-20% on typical archtecture (i86, Alpha, Sparc), and 95% on G4.   CBR also gains but it is little (2% on the typical arch, 30% on G4). - New and improved ATH level autoadjustment. - Use subblock gain more (CBR and VBR). - Fix quantization bug in count1 region, which causes annoying noise in high-freq.

--- libmp3lame/version.h
+++ libmp3lame/version.h
@@ -26,7 +26,7 @@
 
 # define LAME_MAJOR_VERSION      4      /* Major version number */
 # define LAME_MINOR_VERSION      0      /* Minor version number */
-# define LAME_ALPHA_VERSION      13     /* Patch level of alpha version, otherwise zero */
+# define LAME_ALPHA_VERSION      14     /* Patch level of alpha version, otherwise zero */
 # define LAME_BETA_VERSION       0      /* Patch level of beta  version, otherwise zero */
 
 # define PSY_MAJOR_VERSION       0      /* Major version number */
