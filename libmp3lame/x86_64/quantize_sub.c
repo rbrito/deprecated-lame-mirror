41df5cb304f4e7ccf57cf3069aa4b13d52c58203
Takehiro TOMINAGA <takehiro@users.sf.net>
2005-08-20 16:06:50 +0000

    now it works.

--- libmp3lame/x86_64/quantize_sub.c
+++ libmp3lame/x86_64/quantize_sub.c
@@ -1,6 +1,5 @@
-#include "../encoder.h"
-#include "../util.h"
 #include <xmmintrin.h>
+#include <emmintrin.h>
 
 static const float Qfsqrt2[4] __attribute__((aligned(16))) = {
     SQRT2*0.5,SQRT2*0.5,SQRT2*0.5,SQRT2*0.5};
@@ -18,6 +17,7 @@ lr2ms_SSE(float *pl, float *pr, int i)
 {
     __m128 c = _mm_load_ps(Qfsqrt2);
     int l0, l1, r0, r1;
+
     l0 = ((int*)pl)[i-1];
     l1 = ((int*)pl)[i-2];
     r0 = ((int*)pr)[i-1];
@@ -39,7 +39,7 @@ lr2ms_SSE(float *pl, float *pr, int i)
     }
 }
 
-void sumofsqr_SSE(const float *end, int l, float *res)
+void sumofsqr_SSEx(const float *end, int l, float *res)
 {
     union {
         __m128 m;
@@ -49,7 +49,7 @@ void sumofsqr_SSE(const float *end, int l, float *res)
     __m128 m, s;
     float x;
 
-    float *p = end+l;
+    const float *p = end+l;
     m = _mm_loadu_ps(p);
     s = _mm_xor_ps(s, s);
     if (l & 2) {
@@ -74,67 +74,73 @@ void sumofsqr_SSE(const float *end, int l, float *res)
 }
 
 void
-pow075_SSE(float *xr, float *xr34, int end, float *max)
+pow075_SSE(float *x, float *x34, int end, float *max)
 {
-    int i = 0;
     __m128 srmax, zero;
     __m128 fm0p25 = _mm_load_ps(Qfm0p25);
     zero = srmax = _mm_xor_ps(srmax, srmax);
-    
-    for (i = 0; i < end; i += 8) {
-	__mm128 w0, w1;
-	__mm128 s0, s1, s2;
-	w0 = _mm_load_ps(xr);
-	w1 = _mm_load_ps(xr+4);
-	w0 = _mm_and_ps(w0, Q_ABS);
-	w1 = _mm_and_ps(w1, Q_ABS);
-	_mm_store_ps(xr + 576  , w0);
-	_mm_store_ps(xr + 576+4, w1);
-	xr += 8;
+
+    do {
+	__m128 w0, w1;
+	__m128 s0, s1, s2;
+	w0 = _mm_load_ps(x);
+	w1 = _mm_load_ps(x+4);
+	w0 = _mm_and_ps(w0, *(__m128*)Q_ABS);
+	w1 = _mm_and_ps(w1, *(__m128*)Q_ABS);
+	_mm_store_ps(x34 + 576  , w0);
+	_mm_store_ps(x34 + 576+4, w1);
+	x += 8;
 
 	s0 = _mm_rsqrt_ps(w0);
 	srmax = _mm_max_ps(srmax, w0);
-	s1 = s0;
+	s1 = w0;
 	srmax = _mm_max_ps(srmax, w1);
 	s0 = _mm_rsqrt_ps(s0);
+	s1 = _mm_mul_ps(s1, fm0p25);
+	s0 = _mm_rcp_ps(s0);
 	s2 = s0;
 	s0 = _mm_mul_ps(s0, s0);
 	w0 = _mm_mul_ps(w0, s2);
 	s1 = _mm_mul_ps(s1, s0);
 	s1 = _mm_mul_ps(s1, s0);
-	s1 = _mm_mul_ps(s1, Q_f1p25);
+	s1 = _mm_add_ps(s1, *(__m128*)Qf1p25);
 	w0 = _mm_mul_ps(w0, s1);
 
 	s0 = _mm_rsqrt_ps(w1);
-	s1 = s0;
+	s1 = w1;
 	s0 = _mm_rsqrt_ps(s0);
+	s1 = _mm_mul_ps(s1, fm0p25);
+	s0 = _mm_rcp_ps(s0);
 	s2 = s0;
 	s0 = _mm_mul_ps(s0, s0);
 	w1 = _mm_mul_ps(w1, s2);
 	s1 = _mm_mul_ps(s1, s0);
 	s1 = _mm_mul_ps(s1, s0);
-	s1 = _mm_mul_ps(s1, Q_f1p25);
+	s1 = _mm_add_ps(s1, *(__m128*)Qf1p25);
 	w1 = _mm_mul_ps(w1, s1);
 
 	w0 = _mm_max_ps(w0, zero);
 	w1 = _mm_max_ps(w1, zero);
-	_mm_store_ps(xr34,   w0);
-	_mm_store_ps(xr34+4, w1);
-	xr34 += 8;
-    }
-    zero = _mm_movhl_ps(srmax);
+	_mm_store_ps(x34,   w0);
+	_mm_store_ps(x34+4, w1);
+	x34 += 8;
+    } while ((end -= 8) > 0);
+
+    _mm_movehl_ps(zero, srmax);
     srmax = _mm_max_ps(srmax, zero);
-    zero = _mm_shuffle_ps(srmax, _MM_SHUFFLE(1,1,1,1));
+    _mm_shuffle_ps(zero, srmax, _MM_SHUFFLE(1,1,1,1));
     srmax = _mm_max_ps(srmax, zero);
-    _mm_store_ss(psum, srmax);
+    _mm_store_ss(max, srmax);
 }
 
-void qnatize_ISO_SSE2(float xrend[], int bw, int sf, int xr34end[])
+void quantize_ISO_SSE2(float xrend[], int bw, int sf, int ixend[])
 {
-    __mm128 istep = _mm_load1_ps(&IPOW20(sf));
-    __mm128 roundfac = _mm_load_ps(Q_ROUNDFAC);
+    float x = IPOW20(sf);
+    __m128 roundfac = _mm_load_ps(Q_ROUNDFAC);
+    __m128 istep = _mm_load1_ps(&x);
+
     do {
-	__mm128 w0, w1, w2, w3;
+	__m128 w0, w1, w2, w3;
 	w0 = _mm_load_ps(xrend+bw   );
 	w1 = _mm_load_ps(xrend+bw+ 4);
 	w2 = _mm_load_ps(xrend+bw+ 8);
@@ -144,13 +150,17 @@ void qnatize_ISO_SSE2(float xrend[], int bw, int sf, int xr34end[])
 	w1 = _mm_mul_ps(w1, istep);
 	w2 = _mm_mul_ps(w2, istep);
 	w3 = _mm_mul_ps(w3, istep);
-	w0 = _mm_cvtps_pd(w0);
-	w1 = _mm_cvtps_pd(w1);
-	w2 = _mm_cvtps_pd(w2);
-	w3 = _mm_cvtps_pd(w3);
-	_mm_store_ps(xr34end+bw-16   );
-	_mm_store_ps(xr34end+bw-16+ 4);
-	_mm_store_ps(xr34end+bw-16+ 8);
-	_mm_store_ps(xr34end+bw-16+12);
+	w0 = _mm_add_ps(w0, roundfac);
+	w1 = _mm_add_ps(w1, roundfac);
+	w2 = _mm_add_ps(w2, roundfac);
+	w3 = _mm_add_ps(w3, roundfac);
+	w0 = (__m128)_mm_cvttps_epi32(w0);
+	w1 = (__m128)_mm_cvttps_epi32(w1);
+	w2 = (__m128)_mm_cvttps_epi32(w2);
+	w3 = (__m128)_mm_cvttps_epi32(w3);
+	_mm_store_ps(ixend+bw-16   , w0);
+	_mm_store_ps(ixend+bw-16+ 4, w1);
+	_mm_store_ps(ixend+bw-16+ 8, w2);
+	_mm_store_ps(ixend+bw-16+12, w3);
     } while (bw < 0);
 }
