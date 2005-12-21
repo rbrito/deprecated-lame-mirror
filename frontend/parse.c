7a7e8d69448b2106a83224027a483dc9efeda65b
Robert Hegemann <robert@users.sf.net>
2005-12-21 21:40:34 +0000

    bug fix: implementation of the priority commandline switch did not what the documentation says (windows only)

--- frontend/parse.c
+++ frontend/parse.c
@@ -159,13 +159,22 @@ SetPriorityClassMacro(DWORD p)
 static void
 setWin32Priority(lame_global_flags * gfp, int Priority)
 {
-    if (Priority > 3) {
-        SetPriorityClassMacro(HIGH_PRIORITY_CLASS);
-        console_printf("==> Priority set to High.\n");
-    }
-    if (Priority < 3) {
+    switch (Priority) {
+    case 0:
+    case 1:
         SetPriorityClassMacro(IDLE_PRIORITY_CLASS);
         console_printf("==> Priority set to Low.\n");
+        break;
+    default:
+    case 2:
+        SetPriorityClassMacro(NORMAL_PRIORITY_CLASS);
+        console_printf("==> Priority set to Normal.\n");
+        break;
+    case 3:
+    case 4:
+        SetPriorityClassMacro(HIGH_PRIORITY_CLASS);
+        console_printf("==> Priority set to High.\n");
+        break;
     }
 }
 #endif
