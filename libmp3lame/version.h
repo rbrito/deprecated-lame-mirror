5aabd1e67d60bd33949b40f688c60a6289902e79
Robert Hegemann <robert@users.sf.net>
2008-04-13 22:03:14 +0000

    updating history, we are now at 3.98 beta 8

--- libmp3lame/version.h
+++ libmp3lame/version.h
@@ -36,13 +36,13 @@
 # define LAME_MAJOR_VERSION      3 /* Major version number */
 # define LAME_MINOR_VERSION     98 /* Minor version number */
 # define LAME_TYPE_VERSION       1 /* 0:alpha 1:beta 2:release */
-# define LAME_PATCH_VERSION      7 /* Patch level */
+# define LAME_PATCH_VERSION      8 /* Patch level */
 # define LAME_ALPHA_VERSION     (LAME_TYPE_VERSION==0)
 # define LAME_BETA_VERSION      (LAME_TYPE_VERSION==1)
 # define LAME_RELEASE_VERSION   (LAME_TYPE_VERSION==2)
 
 # define PSY_MAJOR_VERSION       0 /* Major version number */
-# define PSY_MINOR_VERSION      92 /* Minor version number */
+# define PSY_MINOR_VERSION      93 /* Minor version number */
 # define PSY_ALPHA_VERSION       0 /* Set number if this is an alpha version, otherwise zero */
 # define PSY_BETA_VERSION        0 /* Set number if this is a beta version, otherwise zero */
 
