a75d2f662470f49317ff887e34e7467e646dacec
Rogério Brito <rbrito@ime.usp.br>
2015-02-21 22:57:22 +0000

    test: lametest: Add some PEP-8 love.

--- test/lametest.py
+++ test/lametest.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from string import *
-import os, commands, getopt, sys
+import os
+import commands
+import getopt
+import sys
 
 
 def Usage(mesg):
@@ -23,42 +26,43 @@ def Usage(mesg):
     sys.exit(0)
 
 
-
-##################################################################
+#
 # compare two files, return # bytes which differ and the
 # number of bytes in larger file
-##################################################################
-def fdiff(name1,name2):
-    cmd = "cmp -l "+name1 + " " + name2 + " | wc -l"
+#
+def fdiff(name1, name2):
+    cmd = "cmp -l " + name1 + " " + name2 + " | wc -l"
     # XXX either
     #    use a combination of os.popen + read
     # or
     #    write owen cmp+wc function
     # to replace commands.getoutput
     out = commands.getoutput(cmd)
-    out = split(out,"\n")
-    out=out[-1]
-    if (-1==find(out,"No")):
+    out = split(out, "\n")
+    out = out[-1]
+    if (-1 == find(out, "No")):
         diff = atof(out)
 
-	status = os.access(name1, os.R_OK)
-	if 1 != status:
-	    size1=0
-	else:
+        status = os.access(name1, os.R_OK)
+        if 1 != status:
+            size1 = 0
+        else:
             size1 = os.path.getsize(name1)
         size2 = os.path.getsize(name2)
-        diff = diff + abs(size1-size2)
-        size = max(size1,size2)
+        diff = diff + abs(size1 - size2)
+        size = max(size1, size2)
     else:
         diff = -1
         size = 0
-    return (diff,size)
+    return (diff, size)
 
-##################################################################
+#
 # compare two files, return # bytes which differ and the
 # number of bytes in larger file
-##################################################################
-def compare(name1,name2,decode):
+#
+
+
+def compare(name1, name2, decode):
     if (decode):
         print "converting mp3 to wav for comparison..."
         # XXX shouldn't we use lame1 instead of a hardcoded lame?
@@ -68,40 +72,36 @@ def compare(name1,name2,decode):
         name2 = name2 + ".wav"
 
     rcode = 0
-    diff,size=fdiff(name1,name2)
-    if (diff==0):
-        print "output identical:  diff=%i  total=%i" % (diff,size)
+    diff, size = fdiff(name1, name2)
+    if (diff == 0):
+        print "output identical:  diff=%i  total=%i" % (diff, size)
         rcode = 1
-    elif (diff>0):
-	print "output different: diff=%i  total=%i  %2.0f%%" % \
-        (diff,size,100*float(diff)/size)
+    elif (diff > 0):
+        print "output different: diff=%i  total=%i  %2.0f%%" % \
+            (diff, size, 100 * float(diff) / size)
     else:
-	print "Error comparing files:"
+        print "Error comparing files:"
         print "File 1: " + name1
         print "File 2: " + name2
 
     return rcode
 
 
-
-
-
-
-##################################################################
+#
 # main program
-##################################################################
+#
 try:
     optlist, args = getopt.getopt(sys.argv[1:], 'wm')
 except getopt.error, val:
     Usage('ERROR: ' + val)
 
-decode=0
-lame2="none"
+decode = 0
+lame2 = "none"
 for opt in optlist:
     if opt[0] == '-w':
-        decode=1
+        decode = 1
     elif opt[0] == '-m':
-        lame2="makeref"
+        lame2 = "makeref"
         print os.linesep + "Generating reference output"
 
 
@@ -110,19 +110,19 @@ if len(args) < 3:
 if len(args) > 4:
     Usage("Too many arguments.")
 
-if (lame2=="makeref"):
+if (lame2 == "makeref"):
     if len(args) != 3:
-    	Usage("Too many arguments for -r/-m mode.")
+        Usage("Too many arguments for -r/-m mode.")
 else:
-    if len(args) ==3:
-        lame2="ref"
+    if len(args) == 3:
+        lame2 = "ref"
 
 # populate variables from args and normalize & expand path
-if len(args) >=3:
+if len(args) >= 3:
     options_file = os.path.normpath(os.path.expanduser(args[0]))
     input_file = os.path.normpath(os.path.expanduser(args[1]))
     lame1 = os.path.normpath(os.path.expanduser(args[2]))
-if len(args) >=4:
+if len(args) >= 4:
     lame2 = os.path.normpath(os.path.expanduser(args[3]))
 
 # check readability of options_file
@@ -153,7 +153,7 @@ for x in path:
 if 1 != lame1_ok:
     Usage(lame1 + " is not executable")
 
-if not (lame2=="ref" or lame2=="makeref"):
+if not (lame2 == "ref" or lame2 == "makeref"):
     # check for executable lame2
     for x in path:
         status = os.access(os.path.join(x, lame2), os.X_OK)
@@ -166,60 +166,60 @@ if not (lame2=="ref" or lame2=="makeref"):
 
 tmp = split(options_file, os.sep)
 tmp = tmp[-1]
-basename = replace(input_file,".wav","")
+basename = replace(input_file, ".wav", "")
 basename = basename + "." + tmp
 
 
-num_ok=0
-n=0
-foptions=open(options_file)
+num_ok = 0
+n = 0
+foptions = open(options_file)
 line = rstrip(foptions.readline())
 while line:
-    n = n+1
+    n = n + 1
     name1 = basename + "." + str(n) + ".mp3"
     name2 = basename + "." + str(n) + "ref.mp3"
 
     print      # empty line
 
-    if (lame2=='ref'):
-        cmd = "rm -f "+name1
+    if (lame2 == 'ref'):
+        cmd = "rm -f " + name1
         os.system(cmd)
         cmd = lame1 + " --quiet " + line + " " + input_file + " " + name1
-        print "executable:      ",lame1
-        print "options:         ",line
-        print "input:           ",input_file
-        print "reference output:",name2
+        print "executable:      ", lame1
+        print "options:         ", line
+        print "input:           ", input_file
+        print "reference output:", name2
         print cmd
         os.system(cmd)
-        num_ok = num_ok+compare(name1,name2,decode)
-    elif (lame2=='makeref'):
-        cmd = "rm -f "+name2
+        num_ok = num_ok + compare(name1, name2, decode)
+    elif (lame2 == 'makeref'):
+        cmd = "rm -f " + name2
         os.system(cmd)
-        print "executable: ",lame1
-        print "options:    ",line
-        print "input:      ",input_file
-        print "output:     ",name2
+        print "executable: ", lame1
+        print "options:    ", line
+        print "input:      ", input_file
+        print "output:     ", name2
         cmd = lame1 + " --quiet " + line + " " + input_file + " " + name2
         os.system(cmd)
     else:
-        cmd = "rm -f "+name1
+        cmd = "rm -f " + name1
         os.system(cmd)
-        cmd = "rm -f "+name2
+        cmd = "rm -f " + name2
         os.system(cmd)
-        print "executable:  ",lame1
-        print "executable2: ",lame2
-        print "options:     ",line
-        print "input:       ",input_file
+        print "executable:  ", lame1
+        print "executable2: ", lame2
+        print "options:     ", line
+        print "input:       ", input_file
         cmd1 = lame1 + " --quiet " + line + " " + input_file + " " + name1
         cmd2 = lame2 + " --quiet " + line + " " + input_file + " " + name2
         os.system(cmd1)
         os.system(cmd2)
-	num_ok = num_ok + compare(name1,name2,decode)
+        num_ok = num_ok + compare(name1, name2, decode)
 
     line = rstrip(foptions.readline())
 
 foptions.close()
 
 if lame2 != 'makeref':
-    print os.linesep + "Number of tests which passed: ",num_ok
-    print "Number of tests which failed: ",n-num_ok
+    print os.linesep + "Number of tests which passed: ", num_ok
+    print "Number of tests which failed: ", n - num_ok
